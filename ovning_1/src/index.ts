8d3127f0fc5b2580e222d0f181c9032b73ea0f86
tZandr <alex_tjernstrom@hotmail.com>
2026-03-02 10:16:48 +0100

    interface imported and console logged, exercise complete

--- ovning_1/src/index.ts
+++ ovning_1/src/index.ts
@@ -1,1 +1,19 @@
 import type { Product } from "./types";
+
+const items: Product[] = [
+  {
+    id: 1,
+    name: "Laptop",
+    price: 12999,
+    inStock: true,
+  },
+  {
+    id: 2,
+    name: "Switch",
+    price: 3499,
+    inStock: true,
+    description: "Hybrid video game console from Nintendo.",
+  },
+];
+
+console.log(items)
