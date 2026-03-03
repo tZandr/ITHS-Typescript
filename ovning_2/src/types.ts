127bf8f2b8efdcbc56f36eb1320040e5a2e48e5f
tZandr <alex_tjernstrom@hotmail.com>
2026-03-03 10:39:27 +0100

    interface Description

--- ovning_2/src/types.ts
+++ ovning_2/src/types.ts
@@ -1,2 +1,12 @@
 export type Category = "electronics" | "food" | "clothing";
 export type Price = number | string;
+
+export interface Description {
+  id: number;
+  name: string;
+  price: Price;
+  category: Category;
+  inStock: boolean;
+  description?: string;
+  tags: string[];
+}
