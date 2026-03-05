3d352c8e6ff46cdcf5525c4438c3b9f0ab2502eb
tZandr <alex_tjernstrom@hotmail.com>
2026-03-05 13:16:13 +0100

    Partial & Readonly

--- ovning_4/src/index.ts
+++ ovning_4/src/index.ts
@@ -6,3 +6,10 @@ type idName = Pick<Product, "id" | "name">;
 // Omit
 type noId = Omit<Product, "id">
 
+// Partial(Gör alla fält optional)
+type update = Partial<Product>
+
+// Readonly
+type readonly = Readonly<Product>
+
+
