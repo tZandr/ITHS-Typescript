8836bc251441dda76da150bf7dacd725341e06c1
tZandr <alex_tjernstrom@hotmail.com>
2026-03-05 11:33:49 +0100

    övn 3 klar

--- ovning_3/src/index.ts
+++ ovning_3/src/index.ts
@@ -17,3 +17,25 @@ let returnT = <T>(res: T) => {
 returnT("String");
 returnT(2);
 returnT({ id: 2, name: "Zandr" });
+
+// Generics with Arrays
+let returnArray = <T>(res: T[]) => {
+  return res[0];
+};
+
+// Skapa en wrapper
+const first = returnArray([{ id: 1, name: "object" }, 2, "string"]);
+console.log(first);
+
+let wrapper = <T>(data: T) => {
+  return { data };
+};
+
+console.log(wrapper(1));
+
+// Begränsa en Generic(Bonus)
+const limited = <T extends { id: number }>(data: T) => {
+  return { data };
+};
+
+console.log(limited({ id: 1 }));
