4932cf77ead4c4f47bcb002e16fe932766226481
tZandr <alex_tjernstrom@hotmail.com>
2026-03-06 10:28:39 +0100

    Omits....

--- ovning_4/src/index.ts
+++ ovning_4/src/index.ts
@@ -1,15 +1,60 @@
 import type { Product } from "./types";
 
+const products: Product[] = [
+  {
+    id: 1,
+    name: "Gym Bag",
+    price: 599,
+    description: "For carrying things?",
+    inStock: true,
+    category: ["Accessories"],
+  },
+  {
+    id: 2,
+    name: "Whey 100%",
+    price: 349,
+    description: "Whey Protein Concentrate",
+    inStock: true,
+    category: ["Nutrition", "Supplement"],
+  },
+];
+
+// console.log(products)
+
 // Pick
 type idName = Pick<Product, "id" | "name">;
 
-// Omit
-type noId = Omit<Product, "id">
+const productList: idName[] = [
+  { id: 1, name: "Gym Bag" },
+  { id: 2, name: "Whey 100%" },
+];
+
+console.log("Pick:", productList);
+
+// Omit... seems useless? lol
+type noId = Omit<Product, "id">;
+
+const omitted: noId[] = [
+  {
+    name: "Gym Bag",
+    price: 599,
+    description: "For carrying things?",
+    inStock: true,
+    category: ["Accessories"],
+  },
+  {
+    name: "Whey 100%",
+    price: 349,
+    description: "Whey Protein Concentrate",
+    inStock: true,
+    category: ["Nutrition", "Supplement"],
+  },
+];
+
+console.log("Omit:", omitted);
 
 // Partial(Gör alla fält optional)
-type update = Partial<Product>
+type update = Partial<Product>;
 
 // Readonly
-type readonly = Readonly<Product>
-
-
+type readonly = Readonly<Product>;
